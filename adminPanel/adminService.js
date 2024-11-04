74826a1846c9dc3189958377ab114bb42906a72b
unknown <mechanshil7@gmail.com>
2024-11-04 08:25:46 +0530

    Improved the code, added custom error

--- adminPanel/adminService.js
+++ adminPanel/adminService.js
@@ -2,29 +2,49 @@ import mongoose from 'mongoose';
 import AdminModel from '../model/adminModel.js'
 import bcryt from 'bcrypt'
 
+class CustomError extends Error {
+    constructor(message, errorCode) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+}
+
+/**
+ * Inside AdminService
+ * // adminSignUp (sign up feature for admins)
+ *
+ * // adminsignIn (sign in feature for admins)
+ */
+
 class AdminService {
-    async adminSignUp(adminDetails) {
-        if(!adminDetails || typeof adminDetails !== 'object') {
-            throw new Error("All fields required! - backend");
+    /**
+    * 1. Checks for the adminDetails first (throws a custom error if the validation is not fullfilled!)
+    * 2. Checks if the signup name is already inside the database or not (adminName compares adminDetails.adminName, adminName(database) - adminDetails.adminName(req body))
+    * 3. If there isn't any duplicate account, hash the password provided from the req body using bcryt
+    * 4. Create an account using the provided req body and the hashed password
+    * 5. Throw a custom error if an account cannot be created (throw custom error if cannot signup)
+    * 6. Create a new timestamp (tracking the date & time of account creation (signup date & time))
+    * 7. return the details of the created account
+    */
+    async adminSignUp(adminDetails) { // adminDetails is a req body
+        if (!adminDetails || typeof adminDetails !== 'object') {
+            throw new CustomError("All fields required!(Bad Request) - backend", 400); // throws a custom error in case the req body is not provided fully or the provided req body is not an object
         }
 
-        //check if there's any duplicate account in the database
-        const isDuplicate = await AdminModel.findOne({adminName:adminDetails.adminName});
-        if(isDuplicate) {
-            throw {message:"Account already exist", errorCode:409}
+        const isDuplicate = await AdminModel.findOne({ adminName: adminDetails.adminName }); //check if there's any duplicate account in the database
+        if (isDuplicate) {
+            throw new CustomError("Account already exist!(conflict error) - backend0", 409);
         }
 
-        // encrypted the password
-        const hashPassword = await bcryt.hash(adminDetails.password, 10);
+        const hashPassword = await bcryt.hash(adminDetails.password, 10); // encrypt the password using bcryt
 
-        // create an admin account with adminDetails
-        const account = await AdminModel.create({
+        const account = await AdminModel.create({ // create an admin account with adminDetails(using admin model)
             ...adminDetails,
-            password:hashPassword
+            password: hashPassword
         })
-        // if the account cannot be created, throw an error
-        if(!account) {
-            throw {message:"Account cannot be created! - backend", errorCode:500}
+
+        if (!account) { // if the account cannot be created, throw an error
+            throw new CustomError("Account cannot be created! - backend", 500);
         }
 
         // track the time of an account creation
@@ -32,30 +52,31 @@ class AdminService {
 
         return {
             account,
-            signUpAt:timestamp
+            signUpAt: timestamp
         };
     }
 
+    // change the throw error later with custom error
     async adminSignIn(id, adminDetails) {
         try {
-            if(!id || !mongoose.Types.ObjectId.isValid(id)) {
-                throw{message:"Invalid id - backend", errorCode:400};
+            if (!id || !mongoose.Types.ObjectId.isValid(id)) {
+                throw { message: "Invalid id - backend", errorCode: 400 };
             }
-            if(!adminDetails || typeof adminDetails !== 'object') {
-                throw{message:"All fields required! - backend", errorCode:400};
+            if (!adminDetails || typeof adminDetails !== 'object') {
+                throw { message: "All fields required! - backend", errorCode: 400 };
             }
 
             // have to use .select("+password") since, 'select:false' in database
             const account = await AdminModel.findById(id).select("+password");
-            if(!account) {
-                throw {message:"Account does not exist! - backend", errorCode:404};
+            if (!account) {
+                throw { message: "Account does not exist! - backend", errorCode: 404 };
             }
 
             // compare passwords(enterPassword, storedPassword)
             const comparePassword = await bcryt.compare(adminDetails.password, account.password);
 
-            if(!comparePassword) {
-                throw {message:"Incorrect password! - backend", errorCode:409}
+            if (!comparePassword) {
+                throw { message: "Incorrect password! - backend", errorCode: 409 }
             }
 
             // track the time of an account login
@@ -63,7 +84,7 @@ class AdminService {
 
             return {
                 ...adminDetails,
-                signInAt:timestamp
+                signInAt: timestamp
             }
         } catch (error) {
             console.log(error);
@@ -72,37 +93,39 @@ class AdminService {
     }
 }
 
-export const adminSignUp = async(req, res) => {
-    const {adminDetails} = req.body;
-    const adminService = new AdminService();
+// Controller functions (standalone functions)
+
+export const adminSignUp = async (req, res) => {
+    const { adminDetails } = req.body;
+    const adminService = new AdminService(); // Creating an instance of AdminService class
     try {
-        const response = await adminService.adminSignUp(adminDetails);
+        const response = await adminService.adminSignUp(adminDetails); // Calling the instance method, adminSignUp
 
         return res.status(201).json({
-            message:"Signup successfully! - backend",
+            message: "Signup successfully! - backend",
             response
         })
     } catch (error) {
         res.status(error.errorCode || 500).json({
-            message:error.message || "Internal server error!"
+            message: error.message || "Internal server error!"
         })
     }
 }
 
-export const adminSignIn = async(req, res) => {
-    const {id} = req.params;
-    const {adminDetails} = req.body;
+export const adminSignIn = async (req, res) => {
+    const { id } = req.params;
+    const { adminDetails } = req.body;
     const adminService = new AdminService();
     try {
         const response = await adminService.adminSignIn(id, adminDetails);
 
         return res.status(200).json({
-            message:"Sign in successfully! - backend",
+            message: "Sign in successfully! - backend",
             response
         })
     } catch (error) {
         res.status(error.errorCode || 500).json({
-            message:error.message || "Internal server error! - backend"
+            message: error.message || "Internal server error! - backend"
         })
     }
 }
