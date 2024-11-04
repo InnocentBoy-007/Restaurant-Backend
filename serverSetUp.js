d47e8989081a9c91420fcd1f3fd3eff642f58ea8
unknown <mechanshil7@gmail.com>
2024-11-04 22:20:18 +0530

    Refined the serverSetUp file

--- serverSetUp.js
+++ serverSetUp.js
@@ -1,22 +1,40 @@
 import express from 'express'
-import mongoose from "mongoose";
+import mongoose from "mongoose"; //ODM (Object Data Modeling) library for MongoDB and NodeJs
 import cors from 'cors'
 import dotenv from 'dotenv'
 import route from './route.js';
 
-class CustomError extends Error{
+/**
+ * The CustomError class extends the built-in Error class.
+    It adds an additional property errorCode to provide more context about the error.
+ */
+class CustomError extends Error {
     constructor(message, errorCode) {
-        super(message);
+        super(message); // message is already a property of built-in 'Error'
         this.errorCode = errorCode;
     }
 }
 
-class ServerSetUp{
+class ServerSetUp { // This class encapsulates all the logic related to setting up the server and connecting to the database (encapsulates the main methods)
+    /**
+     * the constructor can be used to declare and initialize instance variables (or "global variables" within the context of the class) that will be accessible throughout the class methods.
+     * However, these variables are not global in the broader sense (like global variables in JavaScript); they are scoped to the instance of the ServerSetUp class.
+     */
     constructor() {
-        dotenv.config();
+        dotenv.config(); // loads environment variables using 'dotenv.config()'
+
+        // instance variables
+        /**
+         * Instance variables (this.PORT, this.MONGO_URL) are used to store configuration values, keeping them private to the instance of the class.
+         * Initializes this.PORT and this.MONGO_URL with values from the environment.
+         */
         this.PORT = process.env.PORT;
         this.MONGO_URL = process.env.MONGO_URL;
-        this.connectServer();
+
+        /**
+         * The instance method 'this.connectServer()' will be automatically called when the instance of the dependant class is created
+         */
+        this.connectServer(); // instance method
     }
 
     async connectDatabase() {
@@ -34,10 +52,9 @@ class ServerSetUp{
             const app = express();
 
             // middlewares
-            app.use(express.json());
-            app.use(cors());
-
-            app.use("/api", route);
+            app.use(express.json()); // parses incoming JSON requests
+            app.use(cors()); // enables CORS for the application
+            app.use("/api", route); // Sets up routing for the API
 
             // Global error handler
             app.use((err, req, res, next) => {
@@ -48,7 +65,7 @@ class ServerSetUp{
                 }
             });
 
-            app.listen(this.PORT || 3000, `0.0.0.0`, ()=> {
+            app.listen(this.PORT || 3000, `0.0.0.0`, () => {
                 console.log(`Database connected at port: ${this.PORT}`)
             })
             console.log("Server setup successfully! - backend");
@@ -59,4 +76,8 @@ class ServerSetUp{
     }
 }
 
-new ServerSetUp();
+/**
+ * The constructor method (constructor()) is called when an instance of the class is created. It initializes the instance variables and starts the server setup process by calling this.connectServer().
+ */
+
+new ServerSetUp(); // instance of the class ServerSetUp
