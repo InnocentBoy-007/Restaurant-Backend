d108527e08dc9d0416f1aec32eec510bfc9c7eb3
unknown <mechanshil7@gmail.com>
2024-11-05 23:24:40 +0530

    Minor adjustment

--- route.js
+++ route.js
@@ -1,24 +1,24 @@
 import express from 'express'
+import { placeOrder,cancelOrder } from './controlOrder/placeRejectOrder.js';
+import { addProduct, updateProduct } from './adminPanel/adminHandleProducts.js';
+import { acceptOrder } from './adminPanel/adminAcceptOrder.js';
+import { rejectOrder } from './adminPanel/adminRejectOder.js';
+import { adminSignUp, adminSignIn } from './adminPanel/adminSignUpSignIn.js';
 const route = express.Router();
 
-import { placeOrder,cancelOrder } from './controlOrder/placeRejectOrder.js';
 route.post("/placeOrder/:id", placeOrder);
 route.delete("/cancelOrder/:id", cancelOrder);
 
-//adminPanel - add or edit the product
-import { addProduct, updateProduct } from './adminPanel/adminHandleProducts.js';
+
 route.post("/addProduct", addProduct);
 route.patch("/updateProduct/:id", updateProduct);
 
-//adminPanel - accept/reject orders
-import { acceptOrder } from './adminPanel/adminAcceptOrder.js';
-import { rejectOrder } from './adminPanel/adminRejectOder.js';
+
 route.patch("/acceptOrder/:id", acceptOrder);
 route.delete("/rejectOrder/:id", rejectOrder);
 
-//admin Signup-SignIn
-import { adminSignUp, adminSignIn } from './adminPanel/adminSignUpSignIn.js';
-route.post("/adminSignUp", adminSignIn);
+
+route.post("/adminSignUp", adminSignUp);
 route.post("/adminSignIn", adminSignIn);
 
 export default route;
--- services/adminService.js
+++ services/adminService.js
@@ -7,14 +7,15 @@ import { CustomError } from '../components/CustomError.js';
 /**
  * Inside AdminService
  * // adminSignUp (sign up feature for admins)
- *
- * // adminsignIn (sign in feature for admins)
+ * // adminSignIn (sign in feature for admins)
+ * // adminAcceptOrder (feature for admins to accept the placed orders)
+ * // adminRejectOrder (feature for admins to reject the placed orders)
  */
 
 export class AdminService {
     /**
     * 1. Checks for the adminDetails first (throws a custom error if the validation is not fullfilled!)
-    * 2. Checks if the signup name is already inside the database or not (adminName compares adminDetails.adminName, adminName(database) - adminDetails.adminName(req body))
+    * 2. Checks if the signup name is already inside the database or not (adminName compares adminDetails.adminName(req body), adminName(database))
     * 3. If there isn't any duplicate account, hash the password provided from the req body using bcryt
     * 4. Create an account using the provided req body and the hashed password
     * 5. Throw a custom error if an account cannot be created (throw custom error if cannot signup)
@@ -51,17 +52,23 @@ export class AdminService {
         };
     }
 
-    // change the throw error later with custom error
-    async adminSignIn(id, adminDetails) {
+    /**
+     * 1. Checks for the adminDetails first (throws a custom error if the validation goes wrong)
+     * 2. Find the account by comparing the stored property adminName with the req.body property, adminName (if true, return the account details along with the password)
+     * 3. If the account is not found while comparing the adminNames, throws a custom error
+     * 4. Since the stored password is encrypted, while comparing the password, the req.body has to be hashed
+     * 5. If the password is wrong, throws another custom error
+     * 6. Added a timestamp to track the time of an account signIn
+     * 7. Finally, return the signedIn account's details along with the timstamp
+     */
+    async adminSignIn(adminDetails) {//{adminDetails} as req.body
         try {
-            if (!id || !mongoose.Types.ObjectId.isValid(id)) throw new CustomError("Invalid Id", 400);
-
             if (!adminDetails || typeof adminDetails !== 'object') {
                 throw new CustomError("All fields required! - backend", 400);
             }
 
             // have to use .select("+password") since, 'select:false' in database
-            const account = await AdminModel.findById(id).select("+password");
+            const account = await AdminModel.findOne({adminName:adminDetails.adminName}).select("+password");
             if (!account) {
                 throw new CustomError("Account does not exist! - backend", 404);
             }
@@ -73,7 +80,7 @@ export class AdminService {
                 throw new CustomError("IncorrectPassword! - backend", 409);
             }
 
-            // track the time of an account login
+            // track the time of an account signIn
             const timestamp = new Date().toLocaleString();
 
             return {
@@ -86,10 +93,17 @@ export class AdminService {
         }
     }
 
+    /**
+     * 1. Validate the orderId first (throws a custom error if it goes wrong)
+     * 2. Find and update the orderDetails using orderId, updates the default status with 'accepted'
+     * 3. If the orderDetails is not found, throw a custom error
+     * 4. Return the updated orderDetails
+     */
     async adminAcceptOrder(orderId) {
         try {
             if(!orderId || !mongoose.Types.ObjectId.isValid(orderId)) throw new CustomError("Invalid Id - backend", 400);
 
+            // It is more conveniet to use {new:true} instead of await order.save() when using .findbyIdAndUpdate
             const order = await OrderDetails.findByIdAndUpdate(orderId,
                 { status: 'accepted' }, // Update the status
                 { new: true } // Return the updated document
@@ -103,15 +117,20 @@ export class AdminService {
         }
     }
 
-    // method to reject order
+    /**
+     * 1. Validates the orderId first (throws a custom error if it goes wrong)
+     * 2. Find the orderDetails and delete it using orderId
+     * 3. If the orderDetails is not found, throws a custom error
+     * 4. Returns the deletion message if the orderDetails deletion is successfull
+     */
     async adminRejectOrder(orderId) {
         try {
             if(!orderId || !mongoose.Types.ObjectId.isValid(orderId)) throw new CustomError("Invalid Id", 400);
 
-            const order = await OrderDetails.findByIdAndDelete(orderId);
+            const order = await OrderDetails.findByIdAndDelete(orderId); // Directly deletes the orderDetails from the database using orderId
             if (!order) throw new CustomError("Order not found!", 404);
 
-            return { message: "Order rejected successfully." };
+            return { message: "Order rejected successfully." }; // Returns only the deletion message without the deleted orderDetails
         } catch (error) {
             throw error;
         }
